0555fa147961e52a7729cfc416cc0ec38364be52
Emmanuel M <emmanuelmmontesinos@gmail.com>
2024-03-24 12:05:03 +0100

    install Update

--- INSTALL.bat
+++ INSTALL.bat
@@ -2,8 +2,12 @@
 
 echo Descargando e instalando Python...
 curl -o python-installer.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe || call :Error "Error al descargar Python"
-python-installer.exe /quiet TargetDir=C:\Python InstallAllUsers=1 PrependPath=1 || call :Error "Error al instalar Python"
+start /wait python-installer.exe /quiet TargetDir=C:\Python InstallAllUsers=1 PrependPath=1 || call :Error "Error al instalar Python"
 del python-installer.exe
+
+echo -------------------------------------------
+echo Instalando Pip
+python -m ensurepip --upgrade
 
 echo Descargando e Instalando Dependencias: ttkbootstrap
 pip install ttkbootstrap || call :Error "Error al instalar ttkbootstrap"
